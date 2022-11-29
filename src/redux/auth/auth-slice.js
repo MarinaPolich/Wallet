7308369d22a8480733b19e98fa00db5b1bb7ec07
Sviatoslava13 <sviatoslavamar@gmail.com>
2022-11-29 17:56:12 +0200

    register and login

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { registration, logIn, logOut, refreshUser } from './auth-operations';
 
 const authSlice = createSlice({
   name: 'auth',
@@ -9,7 +10,50 @@ const authSlice = createSlice({
     isRefreshing: false,
     error: null,
   },
-  extraReducers: {},
+  extraReducers: builder =>
+    builder
+      .addCase(registration.pending, state => {
+        state.isLoggedIn = true;
+      })
+      .addCase(registration.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(registration.rejected, (state, { payload }) => {
+        state.error = payload;
+      })
+      .addCase(logIn.pending, state => {
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logOut.pending, state => {
+        state.isLoggedIn = true;
+      })
+      .addCase(logOut.fulfilled, state => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+      .addCase(logOut.rejected, (state, { payload }) => {
+        state.error = payload;
+      })
+      .addCase(refreshUser.pending, state => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, { payload }) => {
+        state.user = payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, (state, {payload}) => {
+        state.isRefreshing = false;
+        state.error = payload;
+      }),
 });
 
 export const authReducer = authSlice.reducer;
