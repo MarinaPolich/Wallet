c47e16d48a5942f3a61d318492a3fa3e17b3188d
MarinaPolich <35706436+MarinaPolich@users.noreply.github.com>
2022-11-29 06:52:04 +0000

    Deploying to gh-pages from @ MarinaPolich/Wallet@5dea42be86981edd32ce46a59e97855847283741 🚀

---
+++ static/js/982.381e2872.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwallet=self.webpackChunkwallet||[]).push([[982],{648:function(n,e,t){t.r(e),t.d(e,{default:function(){return l}});var u=t(329),r=function(){return(0,u.jsx)("div",{children:"Table"})},l=function(){return(0,u.jsx)(r,{})}}}]);
+//# sourceMappingURL=982.381e2872.chunk.js.map
