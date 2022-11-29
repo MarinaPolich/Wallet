395ecd74241c5037c6c5b6dee496168a44a455fe
MarinaPolich <marina.polich@gmail.com>
2022-11-29 12:46:30 +0200

    add responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
+import { useMobile } from 'hooks/useMobile';
 import { lazy } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
+import { Currency } from './Currency/Currency';
 
 const HomeTab = lazy(() => import('components/HomeTab/HomeTab'));
 const Registration = lazy(() => import('pages/Registration'));
@@ -8,6 +10,7 @@ const DiagramTab = lazy(() => import('components/DiagramTab/DiagramTab'));
 const Dashboard = lazy(() => import('pages/Dashboard/Dashboard'));
 
 export const App = () => {
+  const isMobile = useMobile();
   return (
     <Routes>
       <Route path="login" element={<Login />} />
@@ -15,6 +18,7 @@ export const App = () => {
       <Route path="/" element={<Dashboard />}>
         <Route path="home" element={<HomeTab />} />
         <Route path="diagram" element={<DiagramTab />} />
+        {isMobile && <Route path="currency" element={<Currency />} />}
         <Route path="" element={<Navigate to="home" />} />
       </Route>
       <Route path="*" element={<h1>NotFound</h1>} />
