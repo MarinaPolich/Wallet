cae0f07260bf3fa2a4fe545b2bd6f52d8cc43d6c
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2022-11-30 11:16:52 +0200

    Create redux/currency

---
+++ src/redux/currency/operations.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const apiURL = 'https://api.monobank.ua/bank/currency';
+
+export const fetchCurrency = createAsyncThunk(
+  'bank/currency',
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await axios.get(apiURL);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
