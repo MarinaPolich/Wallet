f2be3526a054f94160095062ff592c0ebbdde02a
MarinaPolich <marina.polich@gmail.com>
2022-12-01 13:49:27 +0200

    add return

--- src/redux/categories/categories-operations.js
+++ src/redux/categories/categories-operations.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-
 export const getAllCategoriesThunk = createAsyncThunk(
-    'categories/fetchAllCategories',
-    async (_, thunkAPI) => {
-      try {
-        const response = await axios.get(
-          'https://wallet.goit.ua/api/transaction-categories'
-        );
-        return response.data;
-      } catch (error) {
-        thunkAPI.rejectWithValue(error.message);
-      }
+  'categories/fetchAllCategories',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        'https://wallet.goit.ua/api/transaction-categories'
+      );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
-  );
+  }
+);
